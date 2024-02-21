ceec5c4e09d8c926d13830ea5e6b97dc0d62008b
Chadha ben Tamrout <chadha.bt@gmail.com>
2024-02-21 10:41:23 +0000

    index.js updates

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -20,42 +20,34 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/:date?", function (req, res) {
-  let date = req.params.date;
-
-  let unixDate;
-  let dateObj;
-  let utcDate;
-
-  // Test whether the input date is a number
-  let isUnix = /^\d+$/.test(date);
-
-  // If no date specified, use the current date
-  if (!date) {
-    dateObj = new Date();
-  }
-  // If the date is a Unix Timestamp
-  else if (date && isUnix) {
-    unixDate = parseInt(date);
-    dateObj = new Date(unixDate);
-  }
-  // If the date is not a unix time stamp
-  else if (date && !isUnix) {
-    dateObj = new Date(date);
-  }
-
-  if (dateObj.toString() === "Invalid Date") {
-    res.json({ error: "Invalid Date" });
-    return;
-  }
-
-  unixDate = dateObj.getTime();
-  utcDate = dateObj.toUTCString();
-
-  res.json({ unix: unixDate, utc: utcDate });
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api", function (req, res) {
+  let currentUnix = new Date().getTime();
+  let currentUtc = new Date().toUTCString();
+  res.json({"unix": currentUnix, "utc": currentUtc});
+});
+/*
+Extra Comments
+*/
+app.get("/api/:date_string", (req, res) => {
+  let dateString = req.params.date_string;
+  let unixPattern = /\d{5,}/;
+    if (unixPattern.test(dateString)) {
+      let dateNum = parseInt(dateString);
+      res.json({"unix": dateNum, "utc": new Date(dateNum).toUTCString()});
+    } else {
+      let inputDate = new Date(dateString);
+      if (inputDate.toString() === "Invalid Date") {
+        res.json({error: "Invalid Date"});
+      } else {
+        res.json({"unix": inputDate.getTime(), "utc": inputDate.toUTCString()});
+      };
+    };
+});
+/* comments */
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
